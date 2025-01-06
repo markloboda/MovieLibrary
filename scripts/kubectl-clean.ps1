5499f16bbe803c468d77759ec426bec635a72340
Mark Loboda <markloboda123@gmail.com>
2025-01-06 17:18:11 +0100

    [UPD] Added missing flask run port set

--- scripts/kubectl-clean.ps1
+++ scripts/kubectl-clean.ps1
@@ -1,4 +1,4 @@
 minikube docker-env | Invoke-Expression
-kubectl delete service login-register-service
-kubectl delete deployment login-register-deployment
+kubectl delete service browse-library
+kubectl delete deployment browse-library-deployment
 kubectl get pods --no-headers -o custom-columns=":metadata.name" | kubectl delete pods
